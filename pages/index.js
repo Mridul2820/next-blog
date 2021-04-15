08504392879e154247e6f28d2575ef7c4e0edeed
Mridul2820 <immridulpanda2820@gmail.com>
2021-04-15 13:36:06 +0530

    fetching data from contentful

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,23 @@
-export default function Recipes() {
+import { createClient } from "contentful";
+
+export async function getStaticProps () {
+    const clint = createClient({
+        space: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+    })
+
+    const res = await clint.getEntries({ content_type: 'cricBlog' })
+
+    return {
+        props: {
+            blogs: res.items
+        }
+    }
+}
+
+export default function Recipes({ blogs }) {
+    console.log(blogs);
+    
     return (
         <div className="recipe-list">
             Recipe List
